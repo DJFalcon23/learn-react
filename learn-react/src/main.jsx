1c2929bdbb6af894bf5fe0836f5e5a6c8c9f94d0
Derek Fields <djfen101@gmial.com>
2022-05-09 17:19:37 -0400

    init custom component

--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -3,24 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const section = (
-  <section>
-    <img width="40" alt="React-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"/>
-    <h1>Fun Facts About React</h1>
-    <ol>
-      <li>Was first released in 2013</li>
-      <li>Was Orginally created by Jordan Walker</li>
-      <li>Has well over 100k stars on GitHub</li>
-      <li>Is maintained by facebook</li>
-      <li>Powers many different types of apps</li>
-    </ol>
-  </section>
-)
+function LikeRecact() {
+  return (<section>
+  <h1>Why I like React</h1>
+  <ol>
+    <li>Its different</li>
+    <li>Theres a larg community behind it</li>
+    <li>It might get me a job</li>
+  </ol>
+</section>)
+}
+  
+
 
 // console.log(navbar)
 // document.getElementById('root').append(navbar);
 
-ReactDOM.createRoot(document.getElementById('root')).render(section)
+ReactDOM.createRoot(document.getElementById('root')).render(<LikeRecact/>)
 
 
 // ReactDOM.render(navbar, document.getElementById("root"))
