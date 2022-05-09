371dc15ae59594139b4888f9c9bd086011b01025
Derek Fields <djfen101@gmial.com>
2022-05-09 17:54:43 -0400

    make elements of page components

--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -3,15 +3,50 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-function LikeRecact() {
-  return (<section>
-  <h1>Why I like React</h1>
-  <ol>
-    <li>Its different</li>
-    <li>Theres a larg community behind it</li>
-    <li>It might get me a job</li>
-  </ol>
-</section>)
+function Header() {
+  return (
+      <header>
+        <nav>
+          <img 
+            width="40" 
+            alt="React-icon" 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
+          />
+        </nav>
+      </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+        <small>© 20xx Fields development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+      <h1>Why I like React</h1>
+      <ol>
+        <li>Its different</li>
+        <li>Theres a larg community behind it</li>
+        <li>It might get me a job</li>
+      </ol>
+    </main>
+    
+  )
+}
+
+function Page() {
+  return (
+    <section>
+      <Header />
+      <MainContent />
+     <Footer />
+    </section>
+  )
 }
   
 
@@ -19,7 +54,7 @@ function LikeRecact() {
 // console.log(navbar)
 // document.getElementById('root').append(navbar);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<LikeRecact/>)
+ReactDOM.createRoot(document.getElementById('root')).render(<Page/>)
 
 
 // ReactDOM.render(navbar, document.getElementById("root"))
