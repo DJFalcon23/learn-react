5b08da2d2095c759ed89fad99a3535e28a81e8cf
Derek Fields <djfen101@gmial.com>
2022-05-09 16:56:33 -0400

    Project 1

--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -3,23 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const navbar = (
-  <nav>
-      <h1>Bob's Bistro</h1>
-      <ul>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-      </ul>
-  </nav>
+const section = (
+  <section>
+    <img width="40" alt="React-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"/>
+    <h1>Fun Facts About React</h1>
+    <ol>
+      <li>Was first released in 2013</li>
+      <li>Was Orginally created by Jordan Walker</li>
+      <li>Has well over 100k stars on GitHub</li>
+      <li>Is maintained by facebook</li>
+      <li>Powers many different types of apps</li>
+    </ol>
+  </section>
 )
 
 // console.log(navbar)
 // document.getElementById('root').append(navbar);
 
-ReactDOM.createRoot(document.getElementById('root')).render(navbar
-  
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(section)
 
 
 // ReactDOM.render(navbar, document.getElementById("root"))
