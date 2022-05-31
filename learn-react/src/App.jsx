6f88fcc3027950bdfdd190071a0d64121070fb16
Derek Fields <djfen101@gmial.com>
2022-05-30 20:58:49 -0400

    add jokes comments

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -12,27 +12,48 @@ import Contact from './components/Contact'
 import Joke from './components/Joke'
 
 function App() {
+
   return (
     <section className='container'>
       <Joke 
         setup="I got my daughter a fridge for her birthday."
         punchline="I can't wait to see her face light up when she opens it."
+        upvotes={23}
+        downvotes={1}
+        isPun={false}
+        comments={[{author: "Joe92374", body: "The funnyest crap EVER!!!", title: "Good Stuff"}, {author: "beaQT66", body: "FUNNY af! So Good!", title: "WOW LOL!!!"}]}
       />
       <Joke 
         setup="How did the hacker escape the police?"
         punchline=" He just ransomware!"
+        upvotes={55}
+        downvotes={12}
+        isPun={false}
+        comments={[{author: "Joe92374", body: "The funnyest crap EVER!!!", title: "Good Stuff"}, {author: "beaQT66", body: "FUNNY af! So Good!", title: "WOW LOL!!!"}]}
       />
       <Joke 
         setup="Why don't pirates travel on mountain roads?"
         punchline="Scurvy."
+        upvotes={87}
+        downvotes={39}
+        isPun={true}
+        comments={[{author: "Joe92374", body: "The funnyest crap EVER!!!", title: "Good Stuff"}, {author: "beaQT66", body: "FUNNY af! So Good!", title: "WOW LOL!!!"}]}
       />
       <Joke 
         setup="Why do bees stay in the hive in the winter?"
         punchline="Swarm"
+        upvotes={38}
+        downvotes={61}
+        isPun={true}
+        comments={[{author: "Joe92374", body: "The funnyest crap EVER!!!", title: "Good Stuff"}, {author: "beaQT66", body: "FUNNY af! So Good!", title: "WOW LOL!!!"}]}
       />
       <Joke 
         setup="What's the best thing about Switzerland?"
         punchline="I don't know, but the flag is a big plus!"
+        upvotes={26}
+        downvotes={14}
+        isPun={true}
+        comments={[{author: "Joe92374", body: "The funnyest crap EVER!!!", title: "Good Stuff"}, {author: "beaQT66", body: "FUNNY af! So Good!", title: "WOW LOL!!!"}]}
       />
     </section>
   )
--- learn-react/src/components/Joke.jsx
+++ learn-react/src/components/Joke.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
 export default function Joke(props) {
+    const punColor = props.isPun && "salmon";
+    // function comments() {
+    //     let commentList = props.comments.reduce((p, c) => {
+    //         let el = (<section>
+    //         <h4>{p.author}</h4>
+    //         <strong>{p.title}</strong>
+    //         <p>{p.body}</p>
+    //     </section>)
+    //         return el.append(c);
+    //     })
+    //     return commentList
+    // }
+    const children = props.comments.map((comm) => (
+        <section>
+            <h4 style={{margin:2+'px'}}>{comm.author}</h4>
+            <span>'<strong>{comm.title}</strong>'</span>
+            <span>: {comm.body}</span>
+            <hr/>
+        </section>
+      ));
+        console.log(children);
     return (
-        <section className='joke'>
-            <h2>{props.setup}</h2>
+        
+        <section className='joke' style={{ background: punColor }}>
+            {props.isPun && <h2>{props.setup}</h2>}
             <p>{props.punchline} 😂😂🤣</p>
+            <p><strong>👍🏾: {props.upvotes}</strong>  <strong>👎🏾: {props.downvotes}</strong></p>
+            <details>
+                <summary>Comments</summary>
+                <div style={{ display: 'flex', flexDirection: 'column',alignItems: "flex-end"}}>
+                {children}
+                </div>
+            </details>
         </section>
     )
 }
